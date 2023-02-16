2a9cdbcb1faeeac410495450827433a806314a01
osrstaurean <osrstaurean@gmail.com>
2023-02-16 13:18:18 -0500

    Initial Commit

--- app_ui.py
+++ app_ui.py
@@ -1,34 +1,34 @@
 import os
 import shutil
 import sys
-
-import self
 from PyQt5.QtCore import *
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import *
 from PyQt5.QtGui import QImage, QPalette, QBrush
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import (
-    QVBoxLayout, QWidget, QPushButton, )
-
+    QVBoxLayout, QWidget, QPushButton )
+from PyQt5 import QtCore, QtGui, QtWidgets, QtSql
+from PyQt5.QtCore import Qt
+from ticket import ticket_system
 import ticket
+import db_test
 
 """
 Main Application User Interface
 """
-class Window(QWidget):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
+class Window(QWidget, QtCore.QAbstractTableModel):
+    def __init__(self):
+        super(Window, self).__init__()
         # Background image location
         oImage = QImage("E:\School\CEN4031.Adv Program Development Framework\TitanHelpApplication\images\\ticket system.png")
         """ Background Image Sizing | Dimensions Height , Width """
         sImage = oImage.scaled(QSize(800, 600))  # resize Image to widgets size 450, 800
         palette = QPalette()
         palette.setBrush(QPalette.Window, QBrush(sImage))
-        self.curr_tickets = ticket.curr_tickets
+
         """ Window Title Name """
         self.setWindowTitle("Titan Help Application")
-
         """ Resize Window """
         self.resize(800, 600)#600, 500
         def __init__(self):
@@ -46,10 +46,10 @@ class Window(QWidget):
         layout = QVBoxLayout()
 
         """ List box for Displaying Tickets """
-        from ticket import ticket_system
-        ticket_list_widget = QListWidget()
-        my_ticket = ticket.curr_tickets
-        my_ticket.append(ticket_system.tickets(self.curr_tickets))
+
+        #ticket_list_widget = QListWidget()
+        #my_ticket = ticket.curr_tickets
+        #my_ticket.append(ticket_system.tickets(self.curr_tickets))
         # function for filling list widget if ticket_list has more than 0 entries
 
 
@@ -57,47 +57,48 @@ class Window(QWidget):
         # Create the tab widget
         tabs = QTabWidget()
 
-        """New ticket button"""
-        new_ticket_btn = QPushButton("Create New Ticket")
-        new_ticket_btn.clicked.connect(ticket.ticket_system.create_ticket)
-        layout.addWidget(new_ticket_btn)
-        layout.addWidget(ticket_list_widget)
+        """ List of Tickets """
+        # open list of tickets in new paginated window
 
-        """refresh list"""
-        refresh_btn = QPushButton("Refresh List")
-        def refresh():
+
+        """New ticket button"""
+        def create_form_win():
+            print('new form clicked')
+            ticket
+        new_ticket_btn = QPushButton("Create Ticket")
+        new_ticket_btn.clicked.connect(create_form_win)
+
+        layout.addWidget(new_ticket_btn)
+        #layout.addWidget(ticket_list_widget)
+
+
+        """ update ticket """
+        refresh_btn = QPushButton("Update Ticket")
+        def update_ticket():
             print(ticket_system.tickets(self.curr_tickets))
-            if len(my_ticket) != 0:
-                ticket_list_widget.addItems(ticket_system.tickets(self.curr_tickets))
-                print(my_ticket, "added to list widget")
-        refresh_btn.clicked.connect(refresh)
+
+        refresh_btn.clicked.connect(update_ticket)
         layout.addWidget(refresh_btn)
 
+        """ Delete ticket """
+        delete_btn = QPushButton("Delete Ticket")
+
+        def delete_ticket():
+            print(ticket_system.tickets(self.curr_tickets))
+
+        delete_btn.clicked.connect(delete_ticket)
+        layout.addWidget(delete_btn)
 
         """ Tab Layout / Widgets """
         #layout.addWidget(new_ticket_btn)
         layout.setAlignment(Qt.AlignRight)
 
-        # tab Close Window Button
-        generalTab.closeButton = QPushButton(generalTab)
-        generalTab.closeButton.setText("Close")  # text
-        generalTab.closeButton.setIcon(QIcon("close.png"))  # icon
-        generalTab.closeButton.setShortcut('Ctrl+D')  # shortcut key
-        generalTab.closeButton.clicked.connect(self.close)
-        generalTab.closeButton.setToolTip("Close the widget")  # Tool tip
-        generalTab.closeButton.move(10, 150)
-        generalTab.setLayout(layout)
-        #return generalTab
+        """ Database """
 
         """Display UI"""
         self.show()
         self.setLayout(layout)
-        print(self.children())
-
-
-
-
-
+#        print(self.children())
 
 
 if __name__ == "__main__":
@@ -105,3 +106,4 @@ if __name__ == "__main__":
     window = Window()
     window.show()
     sys.exit(app.exec_())
+
